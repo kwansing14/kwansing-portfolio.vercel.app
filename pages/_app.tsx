2f64cb4f9c640f9329b539ddf5347ea17186c2aa
kwansing14 <kwansing@hotmail.sg>
2022-09-29 17:56:59 +0800

    yo lets go favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,20 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Kwan Sing&apos;s Portfolio </title>
         <meta name='description' content="KwanSing's Portfolio" />
-        <link rel='icon' href='/favicon.ico' />
+
+        {/* favicon */}
+        <link rel='apple-touch-icon' sizes='120x120' href='/favicon/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
+        <link rel='manifest' href='/favicon/site.webmanifest' />
+        <link rel='mask-icon' href='/favicon/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+
+        <meta property='og:title' content='Kwan Sing Portfolio' />
+        <meta property='og:type' content='website' />
+        <meta property='og:image' content='/favicon/mstile-150x150.png' />
+        <meta property='og:description' content="Kwan Sing's Portfolio" />
       </Head>
       <Component {...pageProps} />
     </Layout>
